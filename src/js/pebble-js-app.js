29ab202f84d472c4e8d7c8c58fa90931e3950adc
Robin <Robinhuett@users.noreply.github.com>
2015-07-16 13:30:11 +0200

    Closes #2 #7

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,6 +1,25 @@
 var initialized = false;
 var options = {};
 
+function GColorFromHex(hex) {
+	var hexNum = parseInt(hex, 16);
+	var a = 192;
+	var r = (((hexNum >> 16) & 0xFF) >> 6) << 4;
+	var g = (((hexNum >>  8) & 0xFF) >> 6) << 2;
+	var b = (((hexNum >>  0) & 0xFF) >> 6) << 0;
+	return a + r + g + b;
+}
+function GColorToHex(color) {
+	var r = (color & 48) >> 4;
+	var g = (color & 12) >> 2;
+	var b = (color & 3) >> 0;
+	var hexString = [r,g,b].map(function (x) {
+		x *= 5;
+		return x.toString(16) + x.toString(16);
+	}).join('');
+	return hexString.toUpperCase();
+}
+
 Pebble.addEventListener("ready", function(e) {
 	console.log("PblKit_called --- ready");
 	initialized = true;
@@ -8,19 +27,21 @@ Pebble.addEventListener("ready", function(e) {
 
 Pebble.addEventListener("showConfiguration", function(e) {
 	console.log("PblKit_called --- showConfiguration");
-	/*if (localStorage.getItem("localOptions") === null)
-	{
-		localStorage.setItem("localOptions", {"KEY_VIBRATEONBT":"on","KEY_SHOWDATE":"off","KEY_BGCOLOR":"gray"});
-	}*/
-	options = JSON.parse(localStorage.getItem("localOptions"));
-	console.log('http://robinhuett.github.io/SoliTime/config.html' + '?' +
-				'vibrateonbt_select=' + options.KEY_VIBRATEONBT + '&' + 
-				'showdate_select=' + options.KEY_SHOWDATE + '&' + 
-				'bgcolor_select=' + options.KEY_BGCOLOR);
-	Pebble.openURL('http://robinhuett.github.io/SoliTime/config.html' + '?' +
-				'vibrateonbt_select=' + options.KEY_VIBRATEONBT + '&' + 
-				'showdate_select=' + options.KEY_SHOWDATE + '&' + 
-				'bgcolor_select=' + options.KEY_BGCOLOR);
+	var localoptions = JSON.parse(localStorage.getItem("localOptions"));
+	console.log('http://robinhuett.github.io/SoliTime/test/config.html' + '?' +
+				'vibrateonbt_select=' + localoptions.KEY_VIBRATEONBT + '&' + 
+				'showdate_select=' + localoptions.KEY_SHOWDATE + '&' + 
+				'boldhour_select=' + localoptions.KEY_BOLDHOUR + '&' + 
+				'primarycolor_select=' + localoptions.KEY_PRIMARYCOLOR + '&' + 
+				'secondarycolor_select=' + localoptions.KEY_SECONDARYCOLOR + '&' + 
+				'backgroundcolor_select=' + localoptions.KEY_BACKGROUNDCOLOR);
+	Pebble.openURL('http://robinhuett.github.io/SoliTime/test/config.html' + '?' +
+				'vibrateonbt_select=' + localoptions.KEY_VIBRATEONBT + '&' + 
+				'showdate_select=' + localoptions.KEY_SHOWDATE + '&' + 
+				'boldhour_select=' + localoptions.KEY_BOLDHOUR + '&' + 
+				'primarycolor_select=' + localoptions.KEY_PRIMARYCOLOR + '&' + 
+				'secondarycolor_select=' + localoptions.KEY_SECONDARYCOLOR + '&' + 
+				'backgroundcolor_select=' + localoptions.KEY_BACKGROUNDCOLOR);
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
@@ -28,11 +49,19 @@ Pebble.addEventListener("webviewclosed", function(e) {
 	//Using primitive JSON validity and non-empty check
 	if (e.response.charAt(0) == "{" && e.response.slice(-1) == "}" && e.response.length > 5) {
 		options = JSON.parse(decodeURIComponent(e.response));
-		console.log("Options = " + JSON.stringify(options));
+		
+		options.KEY_PRIMARYCOLOR = GColorFromHex(options.KEY_PRIMARYCOLOR.substring(2));
+		options.KEY_SECONDARYCOLOR = GColorFromHex(options.KEY_SECONDARYCOLOR.substring(2));
+		options.KEY_BACKGROUNDCOLOR = GColorFromHex(options.KEY_BACKGROUNDCOLOR.substring(2));
 		
 		Pebble.sendAppMessage(options, 
 			function(e) {
 				console.log("Send_Settings --- sending");
+				
+				options.KEY_PRIMARYCOLOR = '0x' + GColorToHex(options.KEY_PRIMARYCOLOR);
+				options.KEY_SECONDARYCOLOR = '0x' + GColorToHex(options.KEY_SECONDARYCOLOR);
+				options.KEY_BACKGROUNDCOLOR = '0x' + GColorToHex(options.KEY_BACKGROUNDCOLOR);
+				
 				localStorage.setItem("localOptions", JSON.stringify(options));
 			}, function(e) {
 				console.log("Send_Settings --- failed");
--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <pebble.h>
+#include <ctype.h>
 #include "dithered_rects.h"
 	
 // Settings
 #define KEY_VIBRATEONBT 0
 #define KEY_SHOWDATE 1
-#define KEY_BGCOLOR 2
+#define KEY_BOLDHOUR 2
+#define KEY_PRIMARYCOLOR 3
+#define KEY_SECONDARYCOLOR 4
+#define KEY_BACKGROUNDCOLOR 5
 	
 static Window *s_main_window;
 static TextLayer *s_hour_1_layer;
@@ -30,67 +34,78 @@ char minute_3_buffer[] = "00";
 char minute_4_buffer[] = "00";
 
 // Color will be set in "init()" depending on the device.
-static GColor s_background_color;
 static GColor s_primary_color;
 static GColor s_secondary_color;
+static GColor s_background_color;
+static GColor s_message_color;
+
+static GFont s_secondary_font;
+static GFont s_primary_font;
+static GFont s_message_font;
 
 
 // General Interface elements
+void updateColors() {
+	text_layer_set_text_color(s_hour_1_layer, s_primary_color);
+	text_layer_set_text_color(s_hour_2_layer, s_primary_color);
+	text_layer_set_text_color(s_serparator_layer, s_primary_color);
+	text_layer_set_text_color(s_minute_1_layer, s_secondary_color);
+	text_layer_set_text_color(s_minute_2_layer, s_secondary_color);
+	text_layer_set_text_color(s_minute_3_layer, s_secondary_color);
+	text_layer_set_text_color(s_minute_4_layer, s_secondary_color);
+	window_set_background_color(s_main_window, s_background_color);
+}
+
 static void main_window_load(Window *window) {
 	// Create hour TextLayers
 	s_hour_1_layer = text_layer_create(GRect(0, 53, 65, 62));
 	text_layer_set_background_color(s_hour_1_layer, GColorClear);
-	text_layer_set_text_color(s_hour_1_layer, s_primary_color);
-	text_layer_set_font(s_hour_1_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_hour_1_layer, s_primary_font);
 	text_layer_set_text(s_hour_1_layer, "19");
 	text_layer_set_text_alignment(s_hour_1_layer, GTextAlignmentRight);
 	s_hour_2_layer = text_layer_create(GRect(0, 159, 65, 62));
 	text_layer_set_background_color(s_hour_2_layer, GColorClear);
-	text_layer_set_text_color(s_hour_2_layer, s_primary_color);
-	text_layer_set_font(s_hour_2_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_hour_2_layer, s_primary_font);
 	text_layer_set_text(s_hour_2_layer, "20");
 	text_layer_set_text_alignment(s_hour_2_layer, GTextAlignmentRight);
 	
 	// Create serparator TextLayers
 	s_serparator_layer = text_layer_create(GRect(65, 50, 14, 65));
 	text_layer_set_background_color(s_serparator_layer, GColorClear);
-	text_layer_set_text_color(s_serparator_layer, s_primary_color);
-	text_layer_set_font(s_serparator_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_serparator_layer, s_secondary_font);
 	text_layer_set_text(s_serparator_layer, ":");
 	text_layer_set_text_alignment(s_serparator_layer, GTextAlignmentCenter);
 	
 	// Create minute TextLayers	
 	s_minute_1_layer = text_layer_create(GRect(79, 0, 65, 62));
 	text_layer_set_background_color(s_minute_1_layer, GColorClear);
-	text_layer_set_text_color(s_minute_1_layer, s_secondary_color);
-	text_layer_set_font(s_minute_1_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_minute_1_layer, s_secondary_font);
 	text_layer_set_text(s_minute_1_layer, "18");
 	text_layer_set_text_alignment(s_minute_1_layer, GTextAlignmentLeft);
 	s_minute_2_layer = text_layer_create(GRect(79, 53, 65, 62));
 	text_layer_set_background_color(s_minute_2_layer, GColorClear);
-	text_layer_set_text_color(s_minute_2_layer, s_secondary_color);
-	text_layer_set_font(s_minute_2_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_minute_2_layer, s_secondary_font);
 	text_layer_set_text(s_minute_2_layer, "19");
 	text_layer_set_text_alignment(s_minute_2_layer, GTextAlignmentLeft);
 	s_minute_3_layer = text_layer_create(GRect(79, 106, 65, 62));
 	text_layer_set_background_color(s_minute_3_layer, GColorClear);
-	text_layer_set_text_color(s_minute_3_layer, s_secondary_color);
-	text_layer_set_font(s_minute_3_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_minute_3_layer, s_secondary_font);
 	text_layer_set_text(s_minute_3_layer, "20");
 	text_layer_set_text_alignment(s_minute_3_layer, GTextAlignmentLeft);
 	s_minute_4_layer = text_layer_create(GRect(79, 159, 65, 62));
 	text_layer_set_background_color(s_minute_4_layer, GColorClear);
-	text_layer_set_text_color(s_minute_4_layer, s_secondary_color);
-	text_layer_set_font(s_minute_4_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48)));
+	text_layer_set_font(s_minute_4_layer, s_secondary_font);
 	text_layer_set_text(s_minute_4_layer, "21");
 	text_layer_set_text_alignment(s_minute_4_layer, GTextAlignmentLeft);
 	
 	// Create message TextLayer
 	s_message_layer = text_layer_create(GRect(0, -50, 144, 50));
 	text_layer_set_background_color(s_message_layer, GColorClear);
-	text_layer_set_text_color(s_message_layer, s_primary_color);
-	text_layer_set_font(s_message_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SEGOEUI_24)));
+	text_layer_set_text_color(s_message_layer, s_message_color);
+	text_layer_set_font(s_message_layer, s_message_font);
 	text_layer_set_text_alignment(s_message_layer, GTextAlignmentCenter);
+	
+	updateColors();
 	
 	// Add it as child layer to the Window's root layer
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_hour_1_layer));
@@ -193,7 +208,7 @@ void update_minutes(struct tm *tick_time) {
 	strftime(minute_2_buffer, sizeof("00"), "%M", tick_time);
 	text_layer_set_text(s_minute_2_layer, minute_2_buffer);
 	
-	// Update TextLayer 2
+	// Update TextLayer 3
 	if(tick_time->tm_min == 59) {
 		time_copy.tm_min = 0;
 	}
@@ -206,7 +221,7 @@ void update_minutes(struct tm *tick_time) {
 	strftime(minute_3_buffer, sizeof("00"), "%M", &time_copy);
 	text_layer_set_text(s_minute_3_layer, minute_3_buffer);
 	
-	// Update TextLayer 2
+	// Update TextLayer 4
 	if(tick_time->tm_min == 58) {
 		time_copy.tm_min = 0;
 	}
@@ -317,8 +332,6 @@ void show_message(GColor color, int time, char message[20]) {
 // HANDLE BLUETOOTH CONNECTION
 void bt_handler(bool connected) {
 	if (!connected) {
-		APP_LOG(APP_LOG_LEVEL_INFO, "Phone is not connected!");
-		
 		if (persist_read_bool(KEY_VIBRATEONBT)) {
 			vibes_short_pulse();
 		}
@@ -330,7 +343,29 @@ void bt_handler(bool connected) {
 
 // HANDLE SETTINGS
 void update_settings() {
-	// Here goes color and date or something
+	if (persist_read_bool(KEY_BOLDHOUR)) {
+		s_primary_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_48));
+	}
+	else {
+		s_primary_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48));
+	}
+	text_layer_set_font(s_hour_1_layer, s_primary_font);
+	text_layer_set_font(s_hour_2_layer, s_primary_font);
+
+	
+	#ifdef PBL_COLOR
+		GColor8 color;
+		color.argb = persist_read_int(KEY_PRIMARYCOLOR);
+		s_primary_color = COLOR_FALLBACK(color, GColorBlack);
+	
+		color.argb = persist_read_int(KEY_SECONDARYCOLOR);
+		s_secondary_color = COLOR_FALLBACK(color, GColorWhite);
+	
+		color.argb = persist_read_int(KEY_BACKGROUNDCOLOR);
+		s_background_color = COLOR_FALLBACK(color, GColorClear);
+
+		updateColors();
+	#endif	
 }
 
 static void inbox_received_handler(DictionaryIterator *iterator, void *context) {
@@ -340,29 +375,49 @@ static void inbox_received_handler(DictionaryIterator *iterator, void *context) 
 	while (t != NULL) {
 		switch (t->key) {
 			case KEY_VIBRATEONBT:
-				if (strcmp(t->value->cstring, "on") == 0) {
+				if (strcmp(t->value->cstring, "true") == 0) {
 					APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_VIBRATEONBT = true");
 					
 					persist_write_bool(KEY_VIBRATEONBT, true);
-				} else if (strcmp(t->value->cstring, "off") == 0) {
-					 APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_VIBRATEONBT = false");
+				} else if (strcmp(t->value->cstring, "false") == 0) {
+					APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_VIBRATEONBT = false");
 					
 					persist_write_bool(KEY_VIBRATEONBT, false);
 				}
 				break;
 			case KEY_SHOWDATE:
-				if (strcmp(t->value->cstring, "on") == 0) {
+				if (strcmp(t->value->cstring, "true") == 0) {
 					APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_SHOWDATE = true");
 					
 					persist_write_bool(KEY_SHOWDATE, true);
-				} else if (strcmp(t->value->cstring, "off") == 0) {
+				} else if (strcmp(t->value->cstring, "false") == 0) {
 					APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_SHOWDATE = false");
 					
 					persist_write_bool(KEY_SHOWDATE, false);
 				}
 				break;
-			case KEY_BGCOLOR:
-				APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_BGCOLOR = %s", t->value->cstring);
+			case KEY_BOLDHOUR:
+				if (strcmp(t->value->cstring, "true") == 0) {
+					APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_BOLDHOUR = true");
+					
+					persist_write_bool(KEY_BOLDHOUR, true);
+				} else if (strcmp(t->value->cstring, "false") == 0) {
+					APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_BOLDHOUR = false");
+					
+					persist_write_bool(KEY_BOLDHOUR, false);
+				}
+				break;
+			case KEY_PRIMARYCOLOR:
+				APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_PRIMARYCOLOR = %d", t->value->int8);
+				persist_write_int(KEY_PRIMARYCOLOR, t->value->int32);
+				break;
+			case KEY_SECONDARYCOLOR:
+				APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_SECONDARYCOLOR = %d", t->value->int8);
+				persist_write_int(KEY_SECONDARYCOLOR, t->value->int32);
+				break;
+			case KEY_BACKGROUNDCOLOR:
+				APP_LOG(APP_LOG_LEVEL_DEBUG, "KEY_BACKGROUNDCOLOR = %d", t->value->int8);
+				persist_write_int(KEY_BACKGROUNDCOLOR, t->value->int32);
 				break;
 		}
 		
@@ -386,11 +441,44 @@ static void init() {
 	if (persist_exists(KEY_SHOWDATE) == false) {
 		persist_write_bool(KEY_SHOWDATE, false);
 	}
+	if (persist_exists(KEY_BOLDHOUR) == false) {
+		persist_write_bool(KEY_BOLDHOUR, false);
+	}
+	if (persist_exists(KEY_PRIMARYCOLOR) == false) {
+		persist_write_int(KEY_PRIMARYCOLOR, -64);
+	}
+	if (persist_exists(KEY_SECONDARYCOLOR) == false) {
+		persist_write_int(KEY_SECONDARYCOLOR, -1);
+	}
+	if (persist_exists(KEY_BACKGROUNDCOLOR) == false) {
+		persist_write_int(KEY_BACKGROUNDCOLOR, -43);
+	}
 	
 	// Set colors for Aplite and Basalt. Aplite background is Clear for the dithering layer
-	s_background_color = COLOR_FALLBACK(GColorDarkGray, GColorClear);
-	s_primary_color = COLOR_FALLBACK(GColorBlack, GColorBlack);
-	s_secondary_color = COLOR_FALLBACK(GColorWhite, GColorWhite);
+	#ifdef PBL_COLOR
+		GColor8 color;
+		color.argb = persist_read_int(KEY_PRIMARYCOLOR);
+		s_primary_color = color;
+		color.argb = persist_read_int(KEY_SECONDARYCOLOR);
+		s_secondary_color = color;
+		color.argb = persist_read_int(KEY_BACKGROUNDCOLOR);
+		s_background_color = color;
+	#elif PBL_BW
+		s_primary_color = GColorBlack;
+		s_secondary_color = GColorWhite;
+		s_background_color = GColorClear;
+	#endif
+	s_message_color = COLOR_FALLBACK(GColorBlack, GColorBlack);
+	
+	// Set custom fonts.
+	if (persist_read_bool(KEY_BOLDHOUR)) {
+		s_primary_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_48));
+	}
+	else {
+		s_primary_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48));
+	}
+	s_secondary_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_48));
+	s_message_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_REGULAR_24));
 	
 	// Create main Window element and assign to pointer
 	s_main_window = window_create();
@@ -403,7 +491,7 @@ static void init() {
 	});
 	
 	// Create and fill dithering layer on Aplite
-	#ifdef PBL_PLATFORM_APLITE
+	#ifdef PBL_BW
 		s_aplite_bg_layer = layer_create(GRect(0,0,144,168));
 		layer_set_update_proc(s_aplite_bg_layer, draw_aplite_background);
 	
@@ -428,7 +516,7 @@ static void deinit() {
 	tick_timer_service_unsubscribe();
 	
 	// Destroy dithering layer on Aplite
-	#ifdef PBL_PLATFORM_APLITE
+	#ifdef PBL_BW
 		layer_destroy(s_aplite_bg_layer);
 	#endif
 	
